22b4ea57f57c0f13031d3354a6cb90a617ed1e99
Huzaifa979 <79889534+Huzaifa979@users.noreply.github.com>
2022-01-13 01:29:04 +0500

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,15 @@
+
+public class Node {
+	int age, phonenumber;
+	String name, address, disease, gender;
+	Node left, right;
+	public Node(int kage,int kphonenumber,String kname,String kaddress,String kdisease,String kgender) {
+		age=kage;
+		phonenumber=kphonenumber;
+		name=kname;
+		address=kaddress;
+		disease=kdisease;
+		gender=kgender;
+		left=right=null;
+	}
+}
